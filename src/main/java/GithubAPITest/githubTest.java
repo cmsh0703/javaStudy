36f9f04e7098714be8a5e1e2ab7526b7bffd28d1
cmsh0703 <cmsh0703@gmail.com>
2021-08-06 14:19:25 +0900

    20210806

--- src/main/java/GithubAPITest/githubTest.java
+++ src/main/java/GithubAPITest/githubTest.java
@@ -1,6 +1,7 @@
 package GithubAPITest;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.kohsuke.github.GHIssue;
@@ -20,7 +21,7 @@ public class githubTest {
 		String path = "src/main/resources/application.properties";
 		GitHub github = GitHubBuilder.fromPropertyFile(path).build();
 		// 아래방법도 가능하긴 하나, 비추
-		// GitHub github = new GitHubBuilder().withOAuthToken("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").build();
+		 //GitHub github = new GitHubBuilder().withOAuthToken("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").build();
 		github.checkApiUrlValidity();
 
 		// 해당 repository 가져오기
@@ -29,6 +30,9 @@ public class githubTest {
 		// 모든 issue 객체 가져오기
 		List<GHIssue> issues = repository.getIssues(GHIssueState.ALL);
 
+		//HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		HashMap hm = new HashMap();
+		
 		for(int i= 0; i < issues.size(); i++) {
 			System.out.println("["+(i+1)+" 번째 issue] ===== "+issues.get(i));
 			List<GHIssueComment> issueComment = issues.get(i).getComments();
@@ -38,6 +42,14 @@ public class githubTest {
 				String userName = commentTmp.getUserName();
 				String content = commentTmp.getBody();
 				System.out.print("userName : "+userName+", content : "+content);
+				
+				if(hm.containsKey(userName)){
+					hm.put(userName, Integer.parseInt(hm.get(userName).toString())+1);
+					System.out.println("참석률 증가");
+				}else {
+					hm.put(userName, 1);
+					System.out.println("첫댓글");
+				}
 				
 			}
 			System.out.println();
